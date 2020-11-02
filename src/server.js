3d6a17fa5c04c7e1e4e72e7c2399991d67a64c2d
Lars Brandt <zebrajaeger@zebrajaeger.de>
2020-11-02 23:42:42 +0100

    better variable checks in server

--- src/server.js
+++ src/server.js
@@ -15,8 +15,7 @@ class I2CServer {
             let address = parseNumber(req.query.address);
             let count = parseNumber(req.query.count);
 
-            // TODO check for null and undefined
-            if (address !== null && count !== null) {
+            if (this.isOneOrMoreEmpty(address, count)) {
                 if (this.verbose) {
                     console.log(`read from bus '${busNumber}' at address '${address}' '${count}' bytes`);
                 }
@@ -34,10 +33,9 @@ class I2CServer {
                         res.status(500).send(`${err}`);
                     })
             } else {
-                // TODO check for null and undefined
-                if (address === null && count === null) {
+                if (this.areAllEmpty(address, count)) {
                     res.status(400).send('count and address are missing');
-                } else if (address === null) {
+                } else if (this.isEmpty(address)) {
                     res.status(400).send('address is missing');
                 } else {
                     res.status(400).send('count is missing');
@@ -49,8 +47,7 @@ class I2CServer {
             let address = parseNumber(req.query.address);
             let data = req.query.data;
 
-            // TODO check for null and undefined
-            if (address !== null && data !== null) {
+            if (this.isOneOrMoreEmpty(address, data)) {
                 if (this.verbose) {
                     console.log(`write to bus '${busNumber}' at address '${address}' value '${data}'`)
                 }
@@ -68,10 +65,9 @@ class I2CServer {
                         res.status(500).send(`${err}`);
                     })
             } else {
-                // TODO check for null and undefined
-                if (address === null && data === null) {
+                if (this.areAllEmpty(address, data)) {
                     res.status(400).send('data and address are missing');
-                } else if (address === null) {
+                } else if (this.isEmpty(address)) {
                     res.status(400).send('address is missing');
                 } else {
                     res.status(400).send('data is missing');
@@ -82,6 +78,18 @@ class I2CServer {
         app.listen(port, host, () => {
             console.log(`Listening at http://${host}:${port}`)
         });
+    }
+
+    areAllEmpty(...value) {
+        return value.filter(v => !this.isEmpty(v)).length === 0;
+    }
+
+    isOneOrMoreEmpty(...value) {
+        return value.filter(v => this.isEmpty(v)).length > 0;
+    }
+
+    isEmpty(value) {
+        return (value == null || value.length === 0);
     }
 
     set verbose(verbose) {
