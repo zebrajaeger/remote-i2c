615439e62209c64fbe69f80e1c739fe49dbfac43
Lars Brandt <zebrajaeger@zebrajaeger.de>
2020-11-02 21:46:53 +0100

    fix bus number output

--- src/cli-server.js
+++ src/cli-server.js
@@ -8,7 +8,7 @@ program.option('-b, --busNumber <nr>', 'I²C-Bus number', '0');
 program.option('-p, --port <port>', 'Port to bind on', '8080');
 program.option('-h, --host <hostname>', 'Host, IP, ... to bind on', '0.0.0.0');
 program.parse(process.argv);
-console.log('bus', program.bus)
+console.log('bus', program.busNumber)
 console.log('server', program.host + ':' + program.port)
 
 const server = new I2CServer(program.host, parseInt(program.port), parseInt(program.busNumber));
